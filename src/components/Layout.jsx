247000af63821fd061b9fc8f2e51cb7a08812395
Mario Parreño <mario-andree@hotmail.com>
2024-06-03 23:37:37 -0500

    another commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Propuesta from "../pages/Propuesta";
 
 function Layout() {
   return (
@@ -9,7 +8,6 @@ function Layout() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/propuesta" element={<Propuesta />} />
           <Route path="*" element={<Navigate to="/" replace={true} />} />
         </Routes>
       </BrowserRouter>
